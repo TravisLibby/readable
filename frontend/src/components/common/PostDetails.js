01797597a3c114f6fdffa734c3c464e20b191c27
Travis Libby <travislibby@Traviss-MacBook-Pro.local>
2018-01-17 08:07:46 -0500

    Add back the Redirect module from react-router-dom.

--- frontend/src/components/common/PostDetails.js
+++ frontend/src/components/common/PostDetails.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Navigation from '../Navigation';
 import Loader from '../common/Loader';
